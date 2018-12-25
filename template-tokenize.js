e275d9ea787baf452195554eb5693d3963da8295
gucong <gucong@gmail.com>
2018-12-25 18:25:36 +0800

    Fix opts of `tokenizer.nextToken()`

--- template-tokenize.js
+++ template-tokenize.js
@@ -2,33 +2,41 @@
 const tokenize = require("postcss/lib/tokenize");
 
 function templateTokenize () {
-	const tokenizer = tokenize.apply(null, arguments);
+	const tokenizer = tokenize.apply(this, arguments);
 
 	function nextToken () {
-		let token = tokenizer.nextToken();
-		if (token && token[0] === "{") {
-			const returned = [token];
-			let depth = 1;
+		const args = arguments;
+		const returned = [];
+		let token;
+		let depth = 0;
+		let line;
+		let column;
 
-			while ((token = tokenizer.nextToken())) {
-				returned.push(token);
-				if (token[0] !== "word") {
-					if (token[0] === "{") {
-						++depth;
-					} else if (token[0] === "}" && --depth <= 0) {
-						break;
-					}
+		while ((token = tokenizer.nextToken.apply(tokenizer, args))) {
+			if (token[0] !== "word") {
+				if (token[0] === "{") {
+					++depth;
+				} else if (token[0] === "}") {
+					--depth;
 				}
 			}
-
-			const lastToken = returned[returned.length - 1];
+			if (depth || returned.length) {
+				line = token[4] || token[2] || line;
+				column = token[5] || token[3] || column;
+				returned.push(token);
+			}
+			if (!depth) {
+				break;
+			}
+		}
+		if (returned.length) {
 			token = [
 				"word",
 				returned.map(token => token[1]).join(""),
 				returned[0][2],
 				returned[0][3],
-				lastToken[2],
-				lastToken[3],
+				line,
+				column,
 			];
 		}
 		return token;
